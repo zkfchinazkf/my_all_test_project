206e1961e1792dacbd24020829e771475332b74e
zkfchinazkf <865382779@qq.com>
2022-04-11 11:32:25 +0800

    repair pthread priority

--- filedir/src/linux/mypthread.cpp
+++ filedir/src/linux/mypthread.cpp
@@ -5,35 +5,36 @@
 #include <string.h>
 #include <sys/prctl.h>
 
+/*
+    1.test pthread sched_priority and sched_mode demo,pthread priority set,set need use root
+
+*/
+
+/*-------test pthread sched_priority and sched_mode demo 1-------------*/
+/*--------------------------------start--------------------------------*/
 void *pthreadfun(void *p)
 {
+    int ret = 0;
     /*Set pthread name*/
     prctl(PR_SET_NAME, "pthreadfunname");
-
     /*Get pthread name*/
     char namebuf[15];
     prctl(PR_GET_NAME,namebuf);
     printf("pthread name is :%s\n",namebuf);
-
     /* get pthread pthread ID*/
     pthread_t selfpth = pthread_self();
-
-    int ret = 0;
-
     /*set pthread priority and mode*/
     sched_param  param;
-    param.sched_priority = 10;
+    param.sched_priority = 60;
     ret = pthread_setschedparam(selfpth,SCHED_RR,&param);
     if(ret)
     {
         printf("pthread_setschedparam fail is %s\n",strerror(errno));
     }
-
     /*get SCHED_RR mode max priority and min priority*/
     int maxpri = sched_get_priority_max(SCHED_RR);
     int minpri = sched_get_priority_min(SCHED_RR);
     printf("maxpri = %d,minpri = %d\n",maxpri,minpri);
-
     /*get pthread priority and mode*/
     sched_param  getparam;
     int          pori;
@@ -43,16 +44,14 @@ void *pthreadfun(void *p)
         printf("pthread_getschedparam fail is %s\n",strerror(errno));
     }
     printf("pori is %d,getparam is %d\n",pori,getparam.sched_priority);
-
     while(1)
     {
         pthread_t selfpth = pthread_self();
-        printf("selfpth = %u\n",selfpth);
-        sleep(3);
+        printf("selfpth = %lu\n",selfpth);
+        sleep(1);
     }
     return NULL;
 }
-
 void *pthreadfun2(void *p)
 {
     pthread_t selfpthread = pthread_self();
@@ -61,73 +60,86 @@ void *pthreadfun2(void *p)
     if (rslt != 0) {
         printf("pthreadfun2 pthread_getattr_np is %s\n",strerror(errno));
     }
-
-    /*pthread inheritance set and get*/
+    /*pthread inheritance get*/
     int mode = PTHREAD_EXPLICIT_SCHED;
     int ret = pthread_attr_getinheritsched(&selfattr,&mode);
     if(ret)
     {
         printf("pthread_attr_getinheritsched fail is %s\n",strerror(errno));
     }
-    mode = PTHREAD_EXPLICIT_SCHED;
-    ret = pthread_attr_setinheritsched(&selfattr, mode);
-    if(ret)
-    {
-        printf("pthread_attr_setinheritsched fail is %s\n",strerror(errno));
-    }
-
-
-    /*pthread pri set and get*/
+    /*pthread pri  get*/
     int policy = SCHED_RR;
     ret = pthread_attr_getschedpolicy(&selfattr, &policy);
     if(ret)
     {
         printf("pthread_attr_getschedpolicy fail is %s\n",strerror(errno));
     }
-    policy = SCHED_RR;
-    ret = pthread_attr_setschedpolicy(&selfattr, policy);
-    if(ret)
-    {
-        printf("pthread_attr_setschedpolicy fail is %s\n",strerror(errno));
-    }
-
-    /*pthread pri set and get*/
+    /*pthread pri get*/
     sched_param  schparam;
-    schparam.sched_priority = 50;
-    ret = pthread_attr_setschedparam(&selfattr,&schparam);
-    if(ret)
-    {
-        printf("pthread_attr_setschedparam fail is %s\n",strerror(errno));
-    }
-
+    schparam.sched_priority = 0;
     ret = pthread_attr_getschedparam(&selfattr,&schparam);
     if(ret)
     {
         printf("pthread_attr_getschedparam fail is %s\n",strerror(errno));
     }
-
-
     int endpoli;
     sched_param endsch;
-    ret = pthread_attr_getschedpolicy(&selfattr, &endpoli);
-    ret = pthread_attr_getschedparam(&selfattr,&endsch);
-
-   
-    printf("pori is %d,getparam is %d\n",endpoli,endsch.sched_priority);
+    ret = pthread_getschedparam(selfpthread,&endpoli,&endsch);
+    if(ret)
+    {
+        printf("pthread_getschedparam fail is %s\n",strerror(errno));
+    }
+    printf("pthreadfun2 pori is %d,getparam is %d\n",endpoli,endsch.sched_priority);
     while(1)
     {
-        // printf("my pori");
-        sleep(1);
+        /*pthread while(1) need do some */
+        for(int idx=0;idx<1000000;idx++)
+        {
+            endpoli = idx * 300+1;
+        }
     }
 }
+/*-------test pthread sched_priority and sched_mode demo 1-------------*/
+/*--------------------------------end----------------------------------*/
+
 
 int main(int argc,char **argv)
 {
-    pthread_t pth1,pth2,pth3;
-    pthread_create(&pth1,NULL,pthreadfun,NULL);
-    pthread_create(&pth2,NULL,pthreadfun2,NULL);
-    printf("pth1 = %u\n",pth1);
-    printf("pth2 = %u\n",pth2);
+/*-------test pthread sched_priority and sched_mode demo 1-------------*/
+/*--------------------------------start--------------------------------*/
+    int ret;
+    pthread_t pth1,pth2;
+    pthread_attr_t selfattr;
+    pthread_attr_init(&selfattr);
+    /*pthread inheritance set*/
+    ret = pthread_attr_setinheritsched(&selfattr, PTHREAD_EXPLICIT_SCHED);
+    if(ret)
+    {
+        printf("pthread_attr_setinheritsched fail is %s\n",strerror(errno));
+    }
+    /*pthread mode set*/
+    ret = pthread_attr_setschedpolicy(&selfattr, SCHED_RR);
+    if(ret)
+    {
+        printf("pthread_attr_setschedpolicy fail is %s\n",strerror(errno));
+    }
+    /*pthread poriority set*/
+    sched_param  schparam;
+    schparam.sched_priority = 80;
+    ret = pthread_attr_setschedparam(&selfattr,&schparam);
+    if(ret)
+    {
+        printf("pthread_attr_setschedparam fail is %s\n",strerror(errno));
+    }
+    /*open next 2 line will lead to data occupation*/
+    // pthread_create(&pth1,NULL,pthreadfun,NULL);
+    // pthread_create(&pth2,&selfattr,pthreadfun2,NULL);
+    printf("pth1 = %lu\n",pth1);
+    printf("pth2 = %lu\n",pth2);
+/*-------test pthread sched_priority and sched_mode demo 1-------------*/
+/*--------------------------------end----------------------------------*/
+
+
     while(1);
     return 0;
 }
